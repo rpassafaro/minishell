4f5d2d32e72f7e8fc01e9853ab3a08f1925ed55b
robert passafaro <rpassafa@e1z3r1p12.42.us.org>
2017-02-16 00:31:22 -0800

    utils normed

--- srcs/utils.c
+++ srcs/utils.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpassafa <rpassafa@student.42.us>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/16 00:31:04 by rpassafa          #+#    #+#             */
+/*   Updated: 2017/02/16 00:31:07 by rpassafa         ###   ########.us       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../headers/minishell.h"
 
-char *subof(char *str, int sub)
+char	*subof(char *str, int sub)
 {
-	int i;
-	char *ret;
+	int		i;
+	char	*ret;
 
 	i = 0;
 	ret = ft_strnew(ft_strlen(str) - sub);
@@ -13,26 +25,25 @@ char *subof(char *str, int sub)
 		i++;
 		sub++;
 	}
-	return(ret);
+	return (ret);
 }
 
-int countarray(char **lst)
+int		countarray(char **lst)
 {
 	int i;
 
 	i = 0;
 	while (lst[i])
 		i++;
-
 	return (i);
 }
 
-void freedub(char **str)
+void	freedub(char **str)
 {
 	int i;
 
 	i = 0;
-	while(str[i] != 0)
+	while (str[i] != 0)
 	{
 		free(str[i]);
 		i++;
