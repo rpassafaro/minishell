842818ac593cdb88d5286eae56f31d5a0e3f2926
robert passafaro <rpassafa@e1z3r1p12.42.us.org>
2017-02-16 00:29:55 -0800

    almost done nornmed runbin

--- srcs/runbin.c
+++ srcs/runbin.c
@@ -1,27 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   runbin.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpassafa <rpassafa@student.42.us>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/16 00:29:30 by rpassafa          #+#    #+#             */
+/*   Updated: 2017/02/16 00:29:32 by rpassafa         ###   ########.us       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../headers/minishell.h"
 #include "../libft/libft.h"
-#include <stdio.h>
 
-
-char **getbins(t_vector *vect)
+char	**getbins(t_vector *vect)
 {
 	int i;
 
 	i = vect->size - 1;
 	while (i > -1)
 	{
-		if (ft_strncmp(*(char **)vectspot(i, vect), "PATH=",4) == 0)
-			return(ft_strsplit(subof(*(char **)vectspot(i, vect), 5), ':'));
+		if (ft_strncmp(*(char **)vectspot(i, vect), "PATH=", 4) == 0)
+			return (ft_strsplit(subof(*(char **)vectspot(i, vect), 5), ':'));
 		i--;
 	}
 	return (NULL);
 }
 
-char *checkbin(char *prog, char *path)
+char	*checkbin(char *prog, char *path)
 {
-	char *test;
-	char *re;
-	struct stat sb;
+	char			*test;
+	char			*re;
+	struct stat		sb;
 
 	re = ft_strjoin(path, "/");
 	test = ft_strjoin(re, prog);
@@ -34,7 +44,7 @@ char *checkbin(char *prog, char *path)
 	else
 	{
 		if ((sb.st_mode & S_IFMT) == S_IFREG)
-			return test;
+			return (test);
 	}
-	return(NULL);
+	return (NULL);
 }
