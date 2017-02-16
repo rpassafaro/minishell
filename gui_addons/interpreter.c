9b1d6466227dda86bd787ba669e2b141329c4416
robert passafaro <rpassafa@e1z3r1p12.42.us.org>
2017-02-15 23:41:49 -0800

    guid addons norms

--- gui_addons/interpreter.c
+++ gui_addons/interpreter.c
@@ -1,22 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   interpreter.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpassafa <rpassafa@student.42.us>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/15 23:40:17 by rpassafa          #+#    #+#             */
+/*   Updated: 2017/02/15 23:41:17 by rpassafa         ###   ########.us       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../headers/minishell.h"
 
-void printfirstpath(char *cwd)
+void	printfirstpath(char *cwd)
 {
 	int i;
 
 	i = ft_strlen(cwd);
 	i--;
-	while(cwd[i] != '/')
+	while (cwd[i] != '/')
 		i--;
 	i++;
-	while(cwd[i])
+	while (cwd[i])
 		ft_putchar(cwd[i++]);
 }
 
-void print_interp()
+void	print_interp(void)
 {
-	char* cwd;
-	char buff[PATH_MAX + 1];
+	char	*cwd;
+	char	buff[PATH_MAX + 1];
 
 	cwd = getcwd(buff, PATH_MAX + 1);
 	ft_putstr("[");
