3905900c5a7bfd1b30f29f982888df9fd6471271
robert passafaro <rpassafa@e1z3r1p12.42.us.org>
2017-02-15 21:37:20 -0800

    added ~ and made smaller still kind of hacky

--- builtins/changedir.c
+++ builtins/changedir.c
@@ -16,6 +16,27 @@ char	*findenvvar(t_vector *vect, char *str)
 	return (NULL);
 }
 
+void changeold(int ret, t_vector *vect, char *cwd, int i)
+{
+	char *str;
+
+	if (ret == 0)
+	{
+		vect_delete(vect, i);
+		str = ft_strjoin("OLDPWD=", cwd);
+		vect_insert(vect, i, &str);
+	}
+}
+
+void checkchange(int i, char *path)
+{
+	if (i == -1)
+	{
+		ft_putstr("cd: no such file or directory: ");
+		ft_putendl(path);
+	}
+}
+
 void	goback(t_vector *vect, char *cwd)
 {
 	char *str;
@@ -29,13 +50,9 @@ void	goback(t_vector *vect, char *cwd)
 	ft_putendl(tmp);
 	free (str);
 	ret = chdir(tmp);
+	checkchange(ret, tmp);
 	free (tmp);
-	if (ret == 0)
-	{
-		vect_delete(vect, i);
-		str = ft_strjoin("OLDPWD=", cwd);
-		vect_insert(vect, i, &str);
-	}
+	changeold(ret, vect, cwd, i);
 }
 void	gohome(t_vector *vect, char *cwd)
 {
@@ -49,24 +66,63 @@ void	gohome(t_vector *vect, char *cwd)
 	tmp = subof(str, 5);
 	free (str);
 	ret = chdir(tmp);
+	checkchange(ret, tmp);
 	free (tmp);
-	if (ret == 0)
+	changeold(ret, vect, cwd, i);
+}
+
+char *til_b(char *path, t_vector *vect)
+{
+	char *str;
+	char *tmp;
+	char *mt;
+
+	str = findenvvar(vect, "HOME");
+	tmp = subof(str, 5);
+	free (str);
+	if (path[0])
+		mt = subof(path, 1);
+	else
+		mt = ft_strdup("");
+	str = ft_strjoin(tmp, mt);
+	free(tmp);
+	free (mt);
+	return (str);
+}
+
+void ft_til(t_vector *vect, char *path, char *cwd)
+{
+	int i;
+	int ret;
+	char *tmp;
+
+	tmp = til_b(path, vect);
+	ft_putendl(tmp);
+	ret = chdir(tmp);
+	checkchange(ret, tmp);
+	free (tmp);
+	if (path[0] && !path[1])
 	{
-		vect_delete(vect, i);
-		str = ft_strjoin("OLDPWD=", cwd);
-		vect_insert(vect, i, &str);
+		gohome(vect,cwd);
+		return;
 	}
+	i = findenvvarint(vect, "OLDPWD");
+	changeold(ret, vect, cwd, i);
 }
 
 void	changedirs(char *path, t_vector *vect)
 {
 	int ret;
-	char *str;
 	int i;
 	char* cwd;
 	char buff[PATH_MAX + 1];
 
 	cwd = getcwd(buff, PATH_MAX + 1);
+	if (path[0] == '~')
+	{
+		ft_til(vect, path, cwd);
+		return;
+	}
 	if (path == NULL)
 	{
 		gohome(vect,cwd);
@@ -79,9 +135,6 @@ void	changedirs(char *path, t_vector *vect)
 	}
 	i = findenvvarint(vect, "OLDPWD");
 	ret = chdir(path);
-	vect_delete(vect, i);
-	str = ft_strjoin("OLDPWD=", cwd);
-	vect_insert(vect, i, &str);
-	//free(cwd);
-	return ;
+	checkchange(ret, path);
+	changeold(ret, vect, cwd, i);
 }
