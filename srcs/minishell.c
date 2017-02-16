7d33f619d0f0e13ee842c3786f4240eddc9eddeb
robert passafaro <rpassafa@e1z3r1p12.42.us.org>
2017-02-15 22:39:10 -0800

    splitting functions

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: rpassafa <rpassafa@student.42.us>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/31 13:13:39 by rpassafa          #+#    #+#             */
-/*   Updated: 2017/02/15 22:12:04 by rpassafa         ###   ########.us       */
+/*   Updated: 2017/02/15 22:38:58 by rpassafa         ###   ########.us       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,12 @@ char	*read_tmp()
 	char	*str1;
 	int		bytes_read;
 	char	*ret;
-	int		dq;
 	t_vector *vect;
 
 	vect = vect_new(10, sizeof(char*));
 	str1 = ft_strnew(BUFF_SIZE);
 	ret = ft_strnew(1);
 	bytes_read = 0;
-	dq = 0;
 	while (ft_strchr(ret, '\n') == NULL)
 	{
 		vect_insert(vect, vect->size, &str1);
@@ -47,6 +45,36 @@ char	*read_tmp()
 	return (ret);
 }
 
+void nonrun(char **temp)
+{
+	ft_putstr("unknown command rsh: ");
+	ft_putendl(temp[0]);
+	freedub(temp);
+}
+
+void dofree(char **temp)
+{
+	temp++;
+	freedub(temp);
+}
+
+void handlearg(int argc, char **argv)
+{
+	if (argc < 0)
+		;
+	if (argv)
+		;
+}
+
+void logicrun(int ret, char **temp, t_vector *vect)
+{
+	if (ret == 1 || ret == -1)
+		freedub(temp);
+	else if(temp[0] && !execprog(temp[0], getbins(vect), temp, vect))
+		nonrun(temp);
+	else
+		dofree(temp);
+}
 
 int main(int argc, char **argv, char **envp)
 {
@@ -55,10 +83,7 @@ int main(int argc, char **argv, char **envp)
 	char **temp;
 	int ret;
 
-	if (argc < 0)
-		;
-	if (argv)
-		;
+	handlearg(argc, argv);
 	vect = vect_new(32, sizeof(char*));
 	storeenv(vect, envp);
 	while(1)
@@ -68,16 +93,9 @@ int main(int argc, char **argv, char **envp)
 		temp = parseinput(str);
 		checkenv(temp, vect);
 		ret = runbuilt(temp, vect);
-		if (ret == 1 || ret == -1)
-		{
-			if (ret == -1)
-				break;
-		}
-		else if(temp[0] && !execprog(temp[0], getbins(vect), temp, vect))
-		{
-			ft_putstr("unknown command rsh: ");
-			ft_putendl(temp[0]);
-		}
+		logicrun(ret, temp, vect);
+		if (ret == -1)
+			break;
 	}
 	exit(0);
 	return 0;
